63d9168ccda8c94b39acff255c5face4dde952ef
bhargavi <bhargavigol97@gmail.com>
2023-08-04 18:04:43 +0530

    first master

---
+++ pr__7.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int sum(int x,int y){
+	return x+y;
+}
+int subtraction(int x,int y){
+	return x-y;
+}
+int multiplication(int x,int y){
+	return x*y;
+}
+int divison (int x,int y){
+	return x/y;
+}
+int per(int x,int y){
+	return x%y;
+}
+}
+void main(){ 
+int a,b ;
+int choice;
+
+printf("enter a and b: ");
+scanf("%d%d",&a,&b);
+
+printf("press 1 for +...\n");
+printf("press 2 for - ...\n");
+printf("press 3 for * ...\n");
+printf("press 4 for /...\n");
+printf("press 5 for %...\n"); 
+
+printf("enter your choice: ");
+scanf("%d",&choice);
+
+switch(choice){
+	case 1:
+		printf("sum of a and b is %d ",sum(a,b));
+		break;
+		
+	case 2:
+	    printf("sum of a and b is %d ",subtraction(a,b));
+		break;	
+		
+		case 3:
+	    printf("sum of a and b is %d ",multipilication(a,b));
+		break;		
+		
+		case 4:
+	    printf("sum of a and b is %d ",divison(a,b));
+		break;		
+		
+		case 5:
+	    printf("sum of a and b is %d ",per(a,b));
+		break;		
+}
+}
